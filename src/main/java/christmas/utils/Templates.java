a28c08e8c511e3349f9f4a1e224a334ad01a59fb
woo <wsk221e@gmail.com>
2023-11-09 19:26:52 +0900

    feat(Templates): 입/출력에 사용되는 문자열 템플릿을 관리하는 enum 클래스 생성

---
+++ src/main/java/christmas/utils/Templates.java
@@ -0,0 +1,25 @@
+package christmas.utils;
+
+public enum Templates {
+    // Values, Fields, Constructor
+    INPUT_DATE_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n"),
+    INPUT_MENU_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n"),
+
+    private final String message;
+
+    Templates(String message) {
+        this.message = message;
+    }
+
+
+    // Features
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    public String format(Object... args) {
+        return String.format(this.message, args);
+    }
+
+}
