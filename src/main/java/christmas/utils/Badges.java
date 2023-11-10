e4c481d35b933193a1c406b20e8a8b7d39003ffd
woo <wsk221e@gmail.com>
2023-11-10 13:42:31 +0900

    feat(Badges): 배지 목록과 배지 수여를 관리하는 enum 클래스 생성 및 배지 수여 단위 테스트 수행

---
+++ src/main/java/christmas/utils/Badges.java
@@ -0,0 +1,27 @@
+package christmas.utils;
+
+public enum Badges {
+    별(5_000),
+    트리(10_000),
+    산타(20_000);
+
+    private int benefit;
+
+    Badges(int benefit) {
+        this.benefit = benefit;
+    }
+
+    public static String getBadgeByBenefit(int benefit) {
+        String highest = "";
+        for (Badges badge : Badges.values()) {
+            if (benefit >= badge.benefit) {
+                highest = badge.toString();
+            }
+            if (benefit < badge.benefit) {
+                break;
+            }
+        }
+        return highest;
+    }
+
+}
---
+++ src/test/java/christmas/utils/BadgesTest.java
@@ -0,0 +1,26 @@
+package christmas.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BadgesTest {
+    @ParameterizedTest
+    @CsvSource({
+            "4999, ''",
+            "5000, '별'",
+            "9999, '별'",
+            "10000, '트리'",
+            "19999, '트리'",
+            "20000, '산타'",
+            "100000, '산타'"
+    })
+    @DisplayName("혜택 금액에 따라 올바른 배지를 부여해야 한다.")
+    void getBadgeByBenefitTest(int benefit, String expectedBadge) {
+        String actualBadge = Badges.getBadgeByBenefit(benefit);
+        assertEquals(expectedBadge, actualBadge);
+    }
+
+}
