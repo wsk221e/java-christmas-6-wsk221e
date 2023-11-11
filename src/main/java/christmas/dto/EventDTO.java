b1af7f5702862013b593424a6a9e78d3fb0a34f8
woo <wsk221e@gmail.com>
2023-11-11 20:20:15 +0900

    fix(EventDTO): 합계 계산 오류 수정
    - 생성자 내에서 합계를 먼저 계산 후 합계 구성 요소를 계산하던 점 수정

--- src/main/java/christmas/dto/EventDTO.java
+++ src/main/java/christmas/dto/EventDTO.java
@@ -3,20 +3,20 @@ package christmas.dto;
 import static christmas.utils.constants.Integers.CHAMPAGNE_PRICE;
 
 public class EventDTO {
-    public final int total, dDay, menu, star;
+    public final int dDay, menu, star, total;
     public final boolean hasChampagne;
     public final String badge;
 
     public EventDTO(int dDay, int menu, int star, boolean champagne, String badge) {
-        this.total = calculateTotal();
         this.dDay = dDay;
         this.menu = menu;
         this.star = star;
         this.hasChampagne = champagne;
         this.badge = badge;
+        this.total = calculateTotal();
     }
 
-    
+
     private int calculateTotal() {
         int total = dDay + menu + star;
         if (hasChampagne) {
